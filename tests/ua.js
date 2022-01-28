6ae2af4f7863abcc0ea7ec56762d6f3fd72efd06
Nathan Vander Wilt <nate@calftrail.com>
2022-01-28 22:40:56 +0000

    migrate to 2-space indent

--- tests/ua.js
+++ tests/ua.js
@@ -1,15 +1,15 @@
 
-var dut = require("../simplify-ua.js"),
+var dut = require("../simplify-ua.js");
     exp = require("./eg.js");
 console.log(exp);
 
 function check(result, intent) {
-    console.log("Result:", result);
-    console.log("Wanted:", intent);
-    console.log("--");
+  console.log("Result:", result);
+  console.log("Wanted:", intent);
+  console.log("--");
 }
 
 exp.forEach(function (d) {
-    let res = dut(d.id);
-    check(res, d.est);
+  let res = dut(d.id);
+  check(res, d.est);
 });
