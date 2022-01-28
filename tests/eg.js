d0fc2c8620283d7da444eb86cdca86f741ea6881
Nathan Vander Wilt <nate@calftrail.com>
2022-01-28 22:52:29 +0000

    start on some more test examples

--- tests/eg.js
+++ tests/eg.js
@@ -1,6 +1,16 @@
-module.exports = [
-    {
-        id: "Mozilla/4.0 (compatible; MSIE 4.01; Windows 98)",
-        est: {}
-    },
-];
+module.exports = [{
+  id: "Mozilla/4.0 (compatible; MSIE 4.01; Windows 98)",
+  est: {}
+},{
+  id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+  est: {},
+},{
+  id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15",
+  est: {},
+},{
+  id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:96.0) Gecko/20100101 Firefox/96.0",
+  est: {},
+},{
+  id: "nope",
+  est: {}
+}];
