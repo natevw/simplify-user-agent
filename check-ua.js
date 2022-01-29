e03839b172f53ef255b5686fe479d0d6f0abe7f0
Nathan Vander Wilt <nate@calftrail.com>
2022-01-29 00:21:21 +0000

    split out tests

--- check-ua.js
+++ check-ua.js
@@ -1,6 +1,5 @@
-// 1.0.0 < 1.0.1, but 1 == 1.0.1
 
-function idEstCompare(aVersion, bVersion) {
+function idEstCompare(aVersion, bVersion) {   // so: 1.0.0 < 1.0.1, but 1 == 1.0.1
   function varr(v) { return v.split('.').map(function (s) { return isNaN(s) ? s : +s; }); }
 
   var aa = varr(aVersion),
@@ -13,15 +12,6 @@ function idEstCompare(aVersion, bVersion) {
   }
   return 0;
 }
-
-console.log("Version comparison check:", [
-  idEstCompare('1.0.0', '1.0.1') === -1,
-  idEstCompare('1.0.1', '1.0.0') === +1,
-  idEstCompare('1', '1.0.1') === 0,
-  idEstCompare('1.0.1', '1') === 0,
-  idEstCompare('', '1') === -1,
-  idEstCompare('1', '') === +1,
-].every(Boolean) ? "okay" : "BAD RESULT!");
 
 function idEstMatch(object, criteria) {
   var CMP = {$at: 0, $lt: -1, $gt: +1};
@@ -38,16 +28,7 @@ function idEstMatch(object, criteria) {
   });
 }
 
-console.log("Match check:", [
-  idEstMatch({v:'3.2.1',app:'UA'}, {$not:{app:'IE'}}),
- !idEstMatch({v:'3.2.1',app:'UA'}, {$not:{app:'UA'}}),
-  idEstMatch({v:'3.2.1',app:'UA'}, {app:'UA'}),
- !idEstMatch({v:'3.2.1',app:'UA'}, {app:'IE'}),
-  idEstMatch({v:'3.2.1',app:'UA'}, {app:'UA', $at:'3.2'}),
- !idEstMatch({v:'3.2.1',app:'UA'}, {app:'UA', $at:'3.1'}),
-  idEstMatch({v:'3.2.1',app:'UA'}, {$gt:'1.2.3'}),
-  idEstMatch({v:'3.2.1',app:'UA'}, {$lt:'3.4.5'}),
-  idEstMatch({v:'3.2.1',app:'UA'}, {$any:[{app:'IE'},{app:'UA'}]}),
-  idEstMatch({v:'3.2.1',app:'UA'}, {$all:[{app:'UA'},{v:'3.2.1'}]}),
-  idEstMatch({v:'3.2.1',app:'UA'}, {$all:[{app:'UA'},{$not:{app:'IE'}}]}),
-].every(Boolean) ? "okay" : "BAD RESULT!");
+if (typeof window === 'undefined') {
+  exports.match = idEstMatch;
+  exports.sortVersion = idEstCompare;
+}
