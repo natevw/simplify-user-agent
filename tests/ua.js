3ecf7745b80795ca39a348fb8b49981d6017abe3
Nathan Vander Wilt <nate@calftrail.com>
2022-01-29 03:44:45 +0000

    use match checker in tests

--- tests/ua.js
+++ tests/ua.js
@@ -1,15 +1,16 @@
 
-var dut = require("../simplify-ua.js");
+var dut = require("..");
     exp = require("./eg.js");
 
 function check(result, intent, orig) {
   console.log(orig);
   console.log("Result:", result);
   console.log("Wanted:", intent);
+  console.log(dut.browserIsMatch(result, intent) ? "ok." : "FAILED!");
   console.log("--");
 }
 
 exp.forEach(function (d) {
-  let res = dut(d.id);
+  let res = dut.simplifyUserAgent(d.id);
   check(res, d.est, d.id);
 });
