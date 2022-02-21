971ec31122cf92fd3922807dc682649967b8a4b4
Nathan Vander Wilt <nate@calftrail.com>
2022-02-21 19:58:10 +0000

    [adds failing tests] flesh out tentative expected results of example UA strings

--- tests/eg.js
+++ tests/eg.js
@@ -1,171 +1,175 @@
 module.exports = [{
   id: "Mozilla/4.0 (compatible; MSIE 4.01; Windows 98)",
-  est: {}
+  est: {browser:"Internet Explorer", version:"4.01", platform: "Windows"},
 },{
   id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"97", platform: "Macintosh"},
 },{
   id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15",
-  est: {},
+  est: {browser:"Safari", version:"15.2", platform: "Macintosh"},
 },{
   id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:96.0) Gecko/20100101 Firefox/96.0",
-  est: {},
+  est: {browser:"Firefox", $at:"96", platform: "Macintosh"},
 },{
   id: "NetNewsWire (RSS Reader; https://netnewswire.com/)",
-  est: {},
+  est: {browser:"NetNewsWire", version:null, platform:null},
+},{
+  // NOTE: for this and a few others following, the developer's intent seems to be for the name to
+  // include white space, i.e. that the browser here is "Feed Wrangler" rather than "Feed".
+  // afaict this is a clear violation of https://datatracker.ietf.org/doc/html/rfc2616#section-3.8
+  // so I'm not particularly inclined to support this — but maybe somehow for pragramatic purposes?
+  id: "Feed Wrangler/1.0 (42 subscribers; feed-id=12334; http://example.com; Allow like Gecko)",
+  est: {browser:"Feed Wrangler", platform:null},
 },{
   id: "Superfeedr bot/2.0 http://example.com - Make your feeds realtime: get in touch - feed-id:123456",
-  est: {},
-},{
-  id: "Go-http-client/1.1",
-  est: {},
-},{
-  id: "curl/7.77.0",
-  est: {},
-},{
-  id: "Mozilla/5.0 (compatible; SemrushBot/7~bl; +http://www.example.com/bot.html)",
-  est: {},
-},{
-  id: "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
-  est: {},
-},{
-  id: "-",
-  est: {},
-},{
-  id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:80.0) Gecko/20100101 Firefox/80.0",
-  est: {},
-},{
-  id: "python-requests/2.27.1",
-  est: {},
-},{
-  id: "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
-  est: {},
-},{
-  id: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
-  est: {},
+  est: {browser:"Superfeedr", platform:null},
 },{
   id: "Sogou web spider/4.0(+http://www.example.com/docs/help/webmasters.htm#07)",
-  est: {},
+  est: {browser:"Sogou", platform:null},
+},{
+  id: "Go-http-client/1.1",
+  est: {browser:"Go-http-client", version:"1.1", platform:null},
+},{
+  id: "curl/7.77.0",
+  est: {browser:"curl", version:"7.77.0", platform:null},
+},{
+  id: "Mozilla/5.0 (compatible; SemrushBot/7~bl; +http://www.example.com/bot.html)",
+  est: {browser:"SemrushBot", $at:"7", platform:null},
+},{
+  id: "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+  est: {browser:"Googlebot", version:"2.1", platform:null},
+},{
+  id: "-",
+  est: {browser:"-", version:null, platform:null},
+},{
+  id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:80.0) Gecko/20100101 Firefox/80.0",
+  est: {browser:"Firefox", $at:"80", platform: "Macintosh"},
+},{
+  id: "python-requests/2.27.1",
+  est: {browser:"python-requests", version:"2.27.1", platform:null},
+},{
+  id: "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
+  est: {browser:"Safari", platform: "iPhone"},
+},{
+  id: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+  est: {browser:"Chrome", $at:"83", platform: "Linux"},
 },{
   id: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 OPR/82.0.4227.50",
-  est: {},
+  est: {browser:"OPR", $at:"82", platform: "Windows"},
 },{
   id: "Podcasts/1575.1.2 CFNetwork/1240.0.4 Darwin/20.6.0",
-  est: {},
+  est: {browser:"Podcasts", version:"1575.1.2"},
 },{
   id: "Feedbin feed-id:4242 - 42 subscribers",
-  est: {},
+  est: {browser:"Feedbin", version:null, platform:null},
 },{
   id: "UniversalFeedParser/5.2.1 +https://code.google.com/p/feedparser/",
-  est: {},
+  est: {browser:"UniversalFeedParser", version:"5.2.1", platform:null},
 },{
   id: "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
-  est: {},
+  est: {browser:"Chrome", $at:"97", platform:"Android"},
 },{
   id: "Mozilla/5.0 (compatible; AhrefsBot/7.0; +http://example.com/robot/)",
-  est: {},
+  est: {browser:"AhrefsBot", version:"7.0", platform:null},
 },{
   id: "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4427.0 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"90", platform:"Windows"},
 },{
   id: "ZoominfoBot (zoominfobot at example dot com)",
-  est: {},
-},{
-  id: "Feed Wrangler/1.0 (42 subscribers; feed-id=12334; http://example.com; Allow like Gecko)",
-  est: {},
+  est: {browser:"ZoominfoBot", platform:null},
 },{
   id: "Mozilla/5.0 (Linux; Android 9; CPH1859) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.98 Mobile Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"97", platform:"Android"},
 },{
   id: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.76",
-  est: {},
+  est: {browser:"Chrome", $at:"97", platform:"Windows"},
 },{
   id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"35", platform:"Macintosh"},
 },{
   id: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0",
-  est: {},
+  est: {browser:"Firefox", $at:"96", platform:"Windows"},
 },{
   id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15 (Applebot/0.1; +http://example.com/go/applebot)",
-  est: {},
+  est: {browser:"Safari", $at:"13.1", platform:"Macintosh"},
 },{
   id: "facebookexternalhit/1.1 (+http://example.com/externalhit_uatext.php)",
-  est: {},
+  est: {browser:"facebookexternalhit", version:"1.1", platform:null},
 },{
   id: "MUCheckTool (unknown version) CFNetwork/1240.0.4 Darwin/20.6.0",
-  est: {},
+  est: {browser:"MUCheckTool", platform:null},
 },{
   id: "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/986368",
-  est: {},
+  est: {browser:"Chrome", $at:"65", platform:"Windows"},
 },{
   id: "Mozilla/5.0 (Linux; Android 7.0;) AppleWebKit/537.36 (KHTML, like Gecko) Mobile Safari/537.36 (compatible; PetalBot;+https://example.com/site/petalbot)",
-  est: {},
+  est: {platform:"Android"},
 },{
   id: "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
-  est: {},
+  est: {browser:"Firefox", $at:"91", platform:"Windows"},
 },{
   id: "Mozilla/5.0 (compatible; BLEXBot/1.0; +http://example.com/)",
-  est: {},
+  est: {browser:"BLEXBot", version:1.0, platform:null},
 },{
   id: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"72", platform:"Windows"},
 },{
   id: "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"72", platform:"Windows"},
 },{
   id: "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.500.29 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"79", platform:null},
 },{
   id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"91", platform:"Macintosh"},
 },{
   id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:44.0) Gecko/20100101 Firefox/44.0",
-  est: {},
+  est: {browser:"Firefox", $at:"44", platform:"Macintosh"},
 },{
   id: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0",
-  est: {},
+  est: {browser:"Firefox", $at:"72", platform:"Linux"},
 },{
   id: "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1",
-  est: {},
+  est: {browser:"Safari", version:"15.1", platform:"iPhone"},
 },{
   id: "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)",
-  est: {},
+  est: {browser:"Internet Explorer", version:"6.0", platform:"Windows"},
 },{
   id: "Mozilla/5.0 (Linux; U; Android 4.4.2; en-US; HM NOTE 1W Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.0.5.850 U3/0.8.0 Mobile Safari/534.30",
-  est: {},
+  est: {platform:"Android"},
 },{
   id: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/97.0.4691.0 Safari/537.36",
-  est: {},
+  est: {browser:"HeadlessChrome", $at:"97", platform:"Linux"},
 },{
   id: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"81", platform:"Linux"},
 },{
   id: "Mozilla/5.0 zgrab/0.x",
-  est: {},
+  est: {browser:"zgrab", $at:"0", platform:null},
 },{
   id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/61.0",
-  est: {},
+  est: {browser:"Firefox", $at:"61", platform:"Macintosh"},
 },{
   id: "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 122.1.3.22.123 (iPhone11,8; iOS 13_2_3; nl_NL; nl-NL; scale=2.00; 1024x768; 12345)",
-  est: {},
+  est: {platform:"iPhone"},
 },{
   id: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"66", platform:"Macintosh"},
 },{
   id: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"60", platform:"Windows"},
 },{
   id: "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36 OPR/36.0.2130.32",
-  est: {},
+  est: {browser:"Chrome", $at:"49", platform:"Windows"},
 },{
   id: "Apache-HttpClient/4.5.2 (Java/1.8.0_151)",
-  est: {},
+  est: {browser:"Apache-HttpClient", version:"4.5.2", platform:null},
 },{
   id: "PHP/5.3.80",
-  est: {},
+  est: {browser:"PHP", $at:"5.3", platform:null},
 },{
   id: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
-  est: {},
+  est: {browser:"Chrome", $at:"94", platform:"Linux"},
 },{
   // see https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/compatibility/ms537503(v=vs.85)
   id: "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
@@ -175,5 +179,5 @@ module.exports = [{
   est: {browser:"Internet Explorer", version:"11.0", platform: "Windows"},
 },{
   id: "nope",
-  est: {}
+  est: {browser:"nope", version:null, platform:null}
 }];
